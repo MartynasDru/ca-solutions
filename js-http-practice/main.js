3d8a181e5ca79e6bb8653a9d236484a2d1c52a38
Martynas Druteika <martynasdd@gmail.com>
2022-05-05 20:05:27 +0300

    js-http-practice solution

--- js-http-practice/main.js
+++ js-http-practice/main.js
@@ -1,4 +1,36 @@
 const tableElementBody = document.querySelector('#table tbody');
+const formElement = document.getElementById('form');
+const nameInputElement = document.getElementById('first-name-input');
+const lastNameInputElement = document.getElementById('last-name-input');
+const universityInputElement = document.getElementById('university-input');
+
+formElement.addEventListener('submit', (e) => {
+    const student = {
+        name: nameInputElement.value,
+        lastName: lastNameInputElement.value,
+        university: universityInputElement.value
+    };
+
+    saveNewStudent(student);
+
+    e.preventDefault();
+});
+
+/// FETCH BELOW
+fetch('http://localhost:3000/students')
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        data.forEach((student) => {
+            const studentRowElement = createStudentRowElement({
+                id: student.id,
+                name: student.name,
+                lastName: student.lastName,
+                university: student.university
+            });
+            tableElementBody.append(studentRowElement);
+        })
+    })
 
 function createStudentRowElement(student) {
     const tableRowElement = document.createElement('tr');
@@ -11,6 +43,10 @@ function createStudentRowElement(student) {
     const tdElement = createTdElement('Remove');
     tableRowElement.append(tdElement);
 
+    tdElement.addEventListener('click', () => {
+        deleteStudent(student.id);
+    });
+
     return tableRowElement;
 }
 
@@ -18,4 +54,20 @@ function createTdElement(text) {
     const tdElement = document.createElement('td');
     tdElement.innerText = text;
     return tdElement;
+}
+
+function saveNewStudent(student) {
+    fetch('http://localhost:3000/students', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(student)
+    });
+}
+
+function deleteStudent(id) {
+    fetch(`http://localhost:3000/students/${id}`, {
+        method: 'DELETE'
+    });
 }
